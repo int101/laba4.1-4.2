90ff04b96d54d7811cef40b08b8fa7fc120ad466
Alexander <Alexanderevrash@yandex.ru>
2022-01-19 20:28:54 +0700

    vvediteB

--- lab4.1/lab4.1/Program.cs
+++ lab4.1/lab4.1/Program.cs
@@ -8,6 +8,25 @@ namespace lab4._1
         {
             Console.WriteLine("Введите первое число");
             int A = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите второе число");
+            int B = int.Parse(Console.ReadLine());
+            Console.WriteLine("Выберите действие: \n1) +\n2) -\n3) *\n4) /");
+            int vari = int.Parse(Console.ReadLine());
+            switch (vari)
+            {
+                case 1:
+                    plus(A, B);
+                    break;
+                case 2:
+                    minus(A, B);
+                    break;
+                case 3:
+                    multy(A, B);
+                    break;
+                case 4:
+                    devide(A, B);
+                    break;
+            }
         }
     }
 }
